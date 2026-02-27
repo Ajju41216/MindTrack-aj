cccab476ab96f61e8bb20eccdfdda740c832d4b2
root <root@ip-172-31-35-67.us-west-2.compute.internal>
2026-02-27 01:06:30 +0000

    updated docker compose

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
+echo "Deploying with Docker Compose..."
 
-TAG=$1
-IMAGE="your-dockerhub-username/brain-tasks-dev:$TAG"
-CONTAINER="brain-tasks-container"
-
-echo "Cleaning up old container..."
-docker stop $CONTAINER || true
-docker rm $CONTAINER || true
-
-echo "Starting new container using image: $IMAGE"
-docker run -d \
-  -p 80:80 \
-  --name $CONTAINER \
-  $IMAGE
+docker-compose up -d --force-recreate
